b54d0b86a03d055ab82bf78ac1c14d901c1f3495
fxshad <fxshad@mail.ru>
2022-10-21 13:38:24 +0700

    'static files workable'

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic import TemplateView
+from django.http import HttpResponse
+
+
+
+
+
+class ContactsView(TemplateView):
+    template_name = 'mainapp/contacts.html'
+
+class CoursesListView(TemplateView):
+    template_name = 'mainapp/courses_list.html'
+
+class DocSiteView(TemplateView):
+    template_name = 'mainapp/doc_site.html'
+
+class IndexView(TemplateView):
+    template_name = 'mainapp/index.html'
+
+class LoginView(TemplateView):
+    template_name = 'mainapp/login.html'
+
+class NewsView(TemplateView):
+    template_name = 'mainapp/news.html'
+
+
+
